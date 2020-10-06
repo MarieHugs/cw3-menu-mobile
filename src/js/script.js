e0d8d42ce8d831381eca79a8864bac18a80331e4
Marie-Eve Lamontagne <marie-evelamontagne1@hotmail.com>
2020-10-06 09:45:22 -0400

    version terminé de l'exercice

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+
+    var hamburger = document.querySelector('.hamburger-animated') ;
+    var menuMobile = document.querySelector(".nav-primary-mobile")
+    hamburger.addEventListener('click', openmenu);
+
+
+    function openmenu(evt){
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if (cible.classList.contains("open")){
+            cible.classList.remove("open");
+            menuMobile.classList.remove("open");
+        }
+        else{
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+    }
+
 });
